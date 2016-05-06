56474543e50f48f612e19a6ff380fdfce5cf7dd7
Jake Varley <jakevarley@gmail.com>
2016-05-05 20:22:38 -0400

    changed rgb encoding

--- src/rgb_renderer.cpp
+++ src/rgb_renderer.cpp
@@ -127,18 +127,19 @@ void RGBRenderer::renderImage(sensor_msgs::Image * img_ptr)
     image = image.convertToFormat(QImage::Format_RGB32);
 //    image.save("saved_image0.png");
 
-
+        int size_of_elem = 3;
 
         if(img_ptr != NULL)
         {
             img_ptr->height = renderArea->getViewportRegion().getWindowSize()[1];
             img_ptr->width = renderArea->getViewportRegion().getWindowSize()[0];
 //            img_ptr->encoding = sensor_msgs::image_encodings::TYPE_8UC4;
-//            img_ptr->data.resize(img_ptr->height*img_ptr->width*sizeof(float));
+//            img_ptr->data.resize(img_ptr->height*img_ptr->width*size_of_elem);
 //            img_ptr->step = img_ptr->width*sizeof(float);
-            img_ptr->encoding = sensor_msgs::image_encodings::RGB8;
-            img_ptr->data.resize(img_ptr->height*img_ptr->width*3);
-            img_ptr->step = img_ptr->width*3;
+            img_ptr->encoding = "rgb8" ;//sensor_msgs::image_encodings::RGB8;
+            //img_ptr->encoding = sensor_msgs::image_encodings::TYPE_32FC3;
+            img_ptr->data.resize(img_ptr->height*img_ptr->width*size_of_elem);
+            img_ptr->step = img_ptr->width*size_of_elem;
             img_ptr->is_bigendian = false;
             img_ptr->header.frame_id ="/graspit_camera";
             img_ptr->header.stamp = ros::Time::now();
@@ -149,11 +150,12 @@ void RGBRenderer::renderImage(sensor_msgs::Image * img_ptr)
               int channels = 3;
               unsigned char * sensor_data = (unsigned char *)img_ptr->data.data();
 
-              for(int y = 0; y < img_ptr->height; ++y, sensor_data+=img_ptr->width*3)
+              for(int y = 0; y < img_ptr->height; ++y, sensor_data+=img_ptr->width*size_of_elem)
               {
                   for(int x = 0; x < img_ptr->width; ++x)
                   {
                       QRgb rgb = image.pixel(x, y);
+                      //qAlpha()
                       sensor_data[x * channels] = qRed(rgb); //r
                       sensor_data[x * channels+1] = qGreen(rgb); //g
                       sensor_data[x * channels+2] = qBlue(rgb); //b
